990fdec18e885897d913c252d347662fd27429e7
davidleonfdez <dleoferu@gmail.com>
2020-02-04 18:00:32 +0000

    Fix comparison helpers

--- core/gen_utils.py
+++ core/gen_utils.py
@@ -18,34 +18,110 @@ def is_listy_or_tensor_array(l):
 
 
 def compare_tensor_lists(l1, l2, progress_tracker):
-    """Returns the indexes where the input lists differ. 
+    """Returns the inequality type and indexes where the input lists differ. 
     
-    Assumes l1 and l2 are listy, which may contain tensors.
+    Assumes l1 and l2 are listy, contain tensors and the leaf values can be
+    obtained with tensor.item().
+
+    Args:
+        progress_tracker: class that exposes, at least, a method notify(self, str) 
+            that may get called multiple times to notify the progress of the comparison
+        
+    Returns:
+        List of tuples, with each one containing:
+            -InequalityType
+            -index path: list of ints
+            -value1 (contained in or related to l1), different than value2
+            -value2 (contained in or related to l2), different than value1
     """
-    _compare_lists_recursive(l1, l2, progress_tracker, [], lambda l, i: l[i], 
-                             lambda a, b: a.item() == b.item())
+    return compare_lists_recursive(l1, l2, progress_tracker, [], lambda l, i: l[i], 
+                                    lambda a, b: a.item() == b.item())
   
 
-def _compare_lists_recursive(l1, l2, progress_tracker, indexes, list_accesor, are_equal):
-    "Assumes l1 and l2 are lists"
+def compare_std_lists(l1, l2, progress_tracker):
+    """Returns the inequality type and indexes where the input lists differ. 
+    
+    Assumes l1 and l2 are built-in Python lists and the leaf values will be compared 
+    directly (l1[i][j]... == l2[i][j]...).
+    Args:
+        progress_tracker: class that exposes, at least, a method notify(self, str) 
+            that may get called multiple times to notify the progress of the comparison.
+
+    Example:
+        compare_std_lists(
+            [[2, 4], [2], [3]], 
+            [[2, 3], [1, 2], [3]], 
+            printer_progress_tracker())
+        Returns:
+            [(<ListsInequalityType.VALUE: 3>, [0, 1], 4, 3),
+             (<ListsInequalityType.LEN: 1>, [1], 1, 2)]
+    """
+    return compare_lists_recursive(l1, l2, progress_tracker, [], lambda l, i: l[i], 
+                                   lambda a, b: a == b)
+
+
+def compare_lists_recursive(l1, l2, progress_tracker, indexes, list_accesor, are_equal):
+    """Returns the inequality type and indexes where the input lists differ. 
+
+    Assumes l1 and l2 are listy. The leaf items are the ones directly compared, by
+    calling are_equal(it1, it2). 
+
+    Args:
+        progress_tracker: class that exposes, at least, a method notify(self, str) 
+            that may get called multiple times to notify the progress of the comparison.
+        indexes: list of ints that will be mutated to get track of the current index.
+            [] should be passed in as its initial value.
+        list_accesor: function that receives a listy object and an index and returns
+            the element in that position.
+        are_equal: function that receives two parameters and returns true if they are
+            deemed equal. Used to compare the leaf items.
+    
+    Returns:
+        List of tuples, with each one containing:
+            -InequalityType
+            -index path: list of ints
+            -value1 (contained in or related to l1), different than value2
+            -value2 (contained in or related to l2), different than value1
+
+    Example:
+        _compare_lists_recursive(
+            [[2, 4], [2], [3]], 
+            [[2, 3], [1, 2], [3]], 
+            printer_progress_tracker(), 
+            [], 
+            lambda l, i: l[i], 
+            lambda a, b: a == b)
+        Returns:
+            [(<ListsInequalityType.VALUE: 3>, [0, 1], 4, 3),
+             (<ListsInequalityType.LEN: 1>, [1], 1, 2)]
+    """
     if len(l1) != len(l2):
-        return [(ListsInequalityType.LEN, indexes)]
+        return [(ListsInequalityType.LEN, indexes.copy(), len(l1), len(l2))]
     is_top_level = indexes==[]
     result = []
     for i in range(len(l1)):
-        if is_top_level: progress_tracker.notify('iteration ' + str(i))
+        if is_top_level: progress_tracker.notify('Iteration ' + str(i))
         l1_i = list_accesor(l1, i)
         l2_i = list_accesor(l2, i)
         if is_listy_or_tensor_array(l1_i) and is_listy_or_tensor_array(l2_i):
             indexes.append(i)
-            child_result = _compare_lists_recursive(l1_i, l2_i, indexes, list_accesor, are_equal)
+            child_result = compare_lists_recursive(l1_i, l2_i, progress_tracker, indexes, list_accesor, are_equal)
             if (child_result is not None): result += child_result
             indexes.pop()
         elif is_listy_or_tensor_array(l1_i) or is_listy_or_tensor_array(l2_i):
-            result.append((ListsInequalityType.TYPE_ASYM, indexes))
+            # Creating a copy of indexes implicitly with + is needed here, in order to get
+            # the current (and not the last) state of indexes to appear in this tuple.
+            result.append((ListsInequalityType.TYPE_ASYM, indexes + [i], type(l1_i), type(l2_i)))
         elif (not are_equal(l1_i, l2_i)):
-            result.append((ListsInequalityType.VALUE, indexes, l1_i, l2_i))
+            # Creating a copy of indexes implicitly with + is needed here, in order to get
+            # the current (and not the last) state of indexes to appear in this tuple.
+            result.append((ListsInequalityType.VALUE, indexes + [i], l1_i, l2_i))
     return result
+
+
+class printer_progress_tracker:
+    def notify(self, message:str):
+        print(message)
 
 
 class Probability(ABC):
@@ -56,6 +132,7 @@ class Probability(ABC):
     @abstractmethod
     def prob(self):
         pass
+
 
 class SingleProbability(BaseProbability):
     def __init__(self, prob_value:float):
