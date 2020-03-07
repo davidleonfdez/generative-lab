ca7aa5a145c4088601c988f67a8c0f2620ae5210
davidleonfdez <dleoferu@gmail.com>
2020-03-07 20:50:57 +0000

    Reorganize folder structure to separate clean notebooks from experiments

--- notebooks/GANs/experiments/midsize_training_set/local_lib_import.py
+++ notebooks/GANs/experiments/midsize_training_set/local_lib_import.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-root_lib_path = os.path.abspath(os.path.join(os.pardir, os.pardir, os.pardir))
+root_lib_path = os.path.abspath(os.path.join(os.pardir, os.pardir, os.pardir, os.pardir))
 if root_lib_path not in sys.path:
     sys.path.insert(0, root_lib_path)
 
