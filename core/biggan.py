45faddda7ccbda4cbb4eeb0ecd9031ad17dde22a
davidleonfdez <dleoferu@gmail.com>
2020-05-21 20:37:07 +0100

    Parametrize init func of BigGAN layers

--- core/biggan.py
+++ core/biggan.py
@@ -1,6 +1,6 @@
 import functools
 import math
-from typing import List, Optional, Tuple
+from typing import Callable, List, Optional, Tuple
 import torch
 import torch.nn as nn
 from torch.nn.utils.spectral_norm import spectral_norm
@@ -55,7 +55,7 @@ def biggan_gen_256(out_n_channels:int=3, ch_mult:int=96, **gen_kwargs):
 class BigGANGenerator(nn.Module):
     def __init__(self, out_sz:int, out_n_channels:int, up_blocks_n_ftrs:List[Tuple[int, int]],
                  z_split_sz:int=20, n_classes:int=1, class_embedding_sz:int=128,
-                 up_op:UpsamplingOperation2d=None):
+                 up_op:UpsamplingOperation2d=None, init_func:Callable=_default_init):
         super().__init__()
 
         self.z_split_sz = z_split_sz
@@ -74,14 +74,14 @@ class BigGANGenerator(nn.Module):
                                 else None)
         self.linear = spectral_norm(nn.Linear(z_split_sz, self.init_sz**2 * up_blocks_n_ftrs[0][0]))
         # Params only passed when present, in order to preserve the defaults of BigResBlockUp
-        up_blocks_kwargs = {}
+        up_blocks_kwargs = {'init_func': init_func}
         if up_op is not None: up_blocks_kwargs['up_op'] = up_op
         self.res_blocks_before_attention = nn.ModuleList([
             BigResBlockUp(in_ch, out_ch, cond_sz, **up_blocks_kwargs) 
             for in_ch, out_ch in up_blocks_n_ftrs[:-1]
         ])
         last_res_block_in_ftrs, last_res_block_out_ftrs = up_blocks_n_ftrs[-1]
-        self.self_att = PooledSelfAttention2d(last_res_block_in_ftrs, init=_default_init)
+        self.self_att = PooledSelfAttention2d(last_res_block_in_ftrs, init=init_func)
         self.last_res_block = BigResBlockUp(last_res_block_in_ftrs, last_res_block_out_ftrs, cond_sz, 
                                             **up_blocks_kwargs)
 
@@ -89,7 +89,7 @@ class BigGANGenerator(nn.Module):
             nn.BatchNorm2d(last_res_block_out_ftrs),
             nn.ReLU(),
             conv_layer(last_res_block_out_ftrs, out_n_channels, bias=False, 
-                       norm_type=NormType.Spectral, use_activ=False, init=_default_init),
+                       norm_type=NormType.Spectral, use_activ=False, init=init_func),
             nn.Tanh()
         )
 
@@ -130,18 +130,18 @@ class BigGANGenerator(nn.Module):
 
 class BigResBlockUp(nn.Module):
     def __init__(self, in_ftrs:int, out_ftrs:int, cond_size:int, up_op:UpsamplingOperation2d=None,
-                 activ:nn.Module=None):
+                 activ:nn.Module=None, init_func:Callable=_default_init):
         super().__init__()
         if activ is None: activ = nn.ReLU()
         if up_op is None: up_op = InterpUpsamplingOp2d()
         conv = functools.partial(conv_layer, bias=False, norm_type=NormType.Spectral, 
-                                 use_activ=False, init=_default_init)
+                                 use_activ=False, init=init_func)
 
-        self.bn1 = ConditionalBatchNorm2d(in_ftrs, cond_size, _default_init, _default_init)
+        self.bn1 = ConditionalBatchNorm2d(in_ftrs, cond_size, init_func, init_func)
         self.activ1 = activ
         self.upsample = up_op.get_layer(in_ftrs, in_ftrs)
         self.conv1 = conv(in_ftrs, out_ftrs)
-        self.bn2 = ConditionalBatchNorm2d(out_ftrs, cond_size, _default_init, _default_init)
+        self.bn2 = ConditionalBatchNorm2d(out_ftrs, cond_size, init_func, init_func)
         self.activ2 = activ
         self.conv2 = conv(out_ftrs, out_ftrs)
 
@@ -202,44 +202,45 @@ def biggan_disc_256(in_n_channels:int=3, ch_mult:int=96, **disc_kwargs):
 
 class BigGANDiscriminator(nn.Module):
     def __init__(self, in_sz:int, res_blocks_n_ftrs:List[Tuple[int, int]], idx_block_self_att:int, 
-                 n_classes:int=1, down_op:DownsamplingOperation2d=None, activ:nn.Module=None):
+                 n_classes:int=1, down_op:DownsamplingOperation2d=None, activ:nn.Module=None,
+                 init_func:Callable=_default_init):
         super().__init__()
 
         self.n_classes = n_classes
         layers = []
 
         if activ is None: activ = nn.ReLU()
-        # Params only passed when present, in order to preserve the defaults of BigResBlockDown
-        down_blocks_kwargs = {}
-        if down_op is not None: down_blocks_kwargs['down_op'] = down_op
+        # down_op only passed when present, in order to preserve the default of BigResBlockDown
+        res_blocks_kwargs = {'activ': activ, 'init_func': init_func}
+        if down_op is not None: res_blocks_kwargs['down_op'] = down_op
+        res_block = functools.partial(BigResBlockDown, **res_blocks_kwargs)
 
         layers.extend([
-            BigResBlockDown(in_ch, out_ch, activ=activ, apply_1st_activ=(i > 0),
-                            **down_blocks_kwargs)
+            res_block(in_ch, out_ch, apply_1st_activ=(i > 0))
             for i, (in_ch, out_ch) in enumerate(res_blocks_n_ftrs[:idx_block_self_att])
         ])
         self_att_n_ch = res_blocks_n_ftrs[idx_block_self_att-1][1]
-        layers.append(PooledSelfAttention2d(self_att_n_ch, init=_default_init))
+        layers.append(PooledSelfAttention2d(self_att_n_ch, init=init_func))
         layers.extend([
-            BigResBlockDown(in_ch, out_ch, activ=activ, **down_blocks_kwargs)
+            res_block(in_ch, out_ch)
             for in_ch, out_ch in res_blocks_n_ftrs[idx_block_self_att:-1]
         ])
         final_n_ftrs = res_blocks_n_ftrs[-1][1]
-        layers.append(BigResBlockDown(*res_blocks_n_ftrs[-1], False, activ=activ))
+        layers.append(res_block(*res_blocks_n_ftrs[-1], downsample=False))
         layers.append(activ)
 
         self.layers = nn.Sequential(*layers)
         self.linear = spectral_norm(nn.Linear(final_n_ftrs, 1))
         self.embed = nn.Embedding(n_classes, final_n_ftrs)
 
-        self._init_weights()
+        self._init_weights(init_func)
 
-    def _init_weights(self):
+    def _init_weights(self, init_func:Callable):
         # Layers inside ResBlocks are initialized in BigResBlockUp()
         # so here we only init the first level layers
         for layer in [self.linear, self.embed]:
             if layer is not None:
-                init_default(layer, _default_init)
+                init_default(layer, init_func)
 
     def forward(self, x, y=None):
         x = self.layers(x)
@@ -256,12 +257,12 @@ class BigGANDiscriminator(nn.Module):
 
 class BigResBlockDown(nn.Module):
     def __init__(self, in_ftrs:int, out_ftrs:int, downsample=True, down_op:DownsamplingOperation2d=None,
-                 activ:nn.Module=None, apply_1st_activ=True):
+                 activ:nn.Module=None, apply_1st_activ=True, init_func:Callable=_default_init):
         super().__init__()
         if activ is None: activ = nn.ReLU()
         if downsample and down_op is None: down_op = AvgPoolHalfDownsamplingOp2d()
         conv = functools.partial(conv_layer, bias=False, norm_type=NormType.Spectral, 
-                                 use_activ=False, init=_default_init)
+                                 use_activ=False, init=init_func)
 
         main_path_layers = []
         if apply_1st_activ: main_path_layers.append(activ)
